33521a283ad63c3608445d82cc0303070375754e
Kirk <kirkrobertsnyder@gmail.com>
2021-02-11 14:44:11 -0500

    Creating a function to build markup

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,9 +1,12 @@
+/* eslint-disable no-unused-vars */
+import axios from 'axios'
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
-
+axios.get('https://api.github.com/users/krsnyder')
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -49,6 +52,56 @@ const followersArray = [];
       </div>
     </div>
 */
+
+function func({avatar_url, name, login, location, html_url, followers_url, following_url, bio}) {
+  let card = document.createElement('div')
+  card.classList.add('card')
+
+  let avatar = document.createElement('img')
+  avatar.setAttribute('src', avatar_url)
+  card.appendChild(avatar)
+
+  let cardInfo = document.createElement('div')
+  cardInfo.classList.add('card-info')
+  card.appendChild(cardInfo)
+
+  let myName = document.createElement('h3')
+  myName.classList.add('name')
+  myName.textContent = name
+  cardInfo.appendChild(myName)
+
+  let userName = document.createElement('p')
+  userName.classList.add('username')
+  userName.textContent = login
+  cardInfo.appendChild(userName)
+
+  let myLocation = document.createElement('p')
+  myLocation.textContent = `Location: ${location}`
+  cardInfo.appendChild(myLocation)
+
+  let link = document.createElement('a')
+  link.setAttribute('src', html_url)
+  link.textContent = html_url
+
+  let profile = document.createElement('p')
+  profile.textContent = `Profile: ${link}`
+  cardInfo.appendChild(profile)
+
+  let follower = document.createElement('p')
+  follower.textContent = `Follower: ${followers_url}`
+  cardInfo.appendChild(follower)
+
+  let following = document.createElement('p')
+  following.textContent = `Following: ${following_url}`
+  cardInfo.appendChild(following)
+
+  let myBio = document.createElement('p')
+  myBio.textContent = `Bio: ${bio}`
+  cardInfo.appendChild(myBio)
+
+  return card
+}
+
 
 /*
   List of LS Instructors Github username's:
